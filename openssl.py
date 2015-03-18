26c5e0ad3fb0750f2cfc5870aff37524e5bdf649
mihasK <mihas-91@tut.by>
2015-03-18 14:32:19 +0300

    color on windows

--- openssl.py
+++ openssl.py
@@ -2,12 +2,13 @@ import subprocess
 import os
 import locale
 from termcolor import colored
-
+from colorama import init
+init()
 
 OPENSSL_DIR = '/home/mihas/openssl/openssl-OpenSSL_1_0_2'
 
-#OPENSSL_EXE = OPENSSL_DIR + '/apps/openssl'
-OPENSSL_EXE = 'C:\dev\openssl-1.0.2-i386-win32\openssl.exe'
+OPENSSL_EXE = OPENSSL_DIR + '/apps/openssl'
+#OPENSSL_EXE = 'C:\dev\openssl-1.0.2-i386-win32\openssl.exe'
 
 os.environ['LD_LIBRARY_PATH'] = OPENSSL_DIR
 
